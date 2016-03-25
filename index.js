7aafae7be35c183de3d0b4cad3a7eb6d96ac41a9
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-25 15:52:20 +0200

    Use npm --parseable instead of json 

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var cp = require('child_process');
 
 // Yanked from http://stackoverflow.com/questions/13981938/print-a-list-of-all-installed-node-js-modules
 function listPackages(callback) {
-    cp.exec('npm ls --json', function (err, stdout, stderr) {
-        if (err) {
-            return callback(err)
-        }
-        callback(null, JSON.parse(stdout));
+    cp.exec('npm ls --parseable', function (err, stdout, stderr) {
+        callback(err, stdout.split('\n'));
     });
 }
 
 listPackages(function (err, out) {
     console.log(out);
+    out.map(function (val) {
+        console.log(val);
+    })
 })
