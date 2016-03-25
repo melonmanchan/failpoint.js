aa79e959e1fb6041029f42dc0bff12830e596838
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-25 15:15:59 +0200

    Add npm package listing 

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
+var cp = require('child_process');
 
+// Yanked from http://stackoverflow.com/questions/13981938/print-a-list-of-all-installed-node-js-modules
+function listPackages(callback) {
+    cp.exec('npm ls --json', function (err, stdout, stderr) {
+        if (err) {
+            return callback(err)
+        }
+        callback(null, JSON.parse(stdout));
+    });
+}
+
+listPackages(function (err, out) {
+    console.log(out);
+})
